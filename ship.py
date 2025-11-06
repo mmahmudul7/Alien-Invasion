006ee5abd967c947235a6f04733a5e9dd16bcb62
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-06 18:13:19 +0600

    Drawing the Ship to the Screen

--- ship.py
+++ ship.py
@@ -3,13 +3,13 @@ import pygame
 class Ship:
     """A class to manage the ship."""
 
-    def __init__(self, ai_game):
+    def __init__(self, ai_game): # ai = AlienInvasion
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen_rect = ai_game.screen.get_rect() # rect = rectangle
 
         # Load the ship image and get its rect. 
-        self.image = pygame.image.load('/images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen. 
