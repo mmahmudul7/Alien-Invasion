c5247df71267747e412b0f16b90dd9930e150e76
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-07 02:32:54 +0600

    Refactoring _check_events()

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-T
+if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True        
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False        
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
