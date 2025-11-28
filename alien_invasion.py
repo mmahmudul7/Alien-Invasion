6751c41c07eb3411872d4e2b0b07ac5f1f7570f6
mmahmudul7 <hasanitgroup@gmail.com>
2025-11-28 21:10:47 +0600

    Making the Fleet Move

--- alien_invasion.py
+++ alien_invasion.py
@@ -126,6 +126,24 @@ class AlienInvasion:
 
         pygame.display.flip()
 
+    def _check_fleet_edges(self):
+        """Response appropriately if any alines have reached edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _update_aliens(self):
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
+        self.aliens.update()
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game. 
